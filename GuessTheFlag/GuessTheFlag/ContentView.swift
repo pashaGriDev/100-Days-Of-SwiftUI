29f757941b8e7f8015f46cee298b6fa6bb6cce4b
Pavel Gri <zlibabaika@gmail.com>
2023-07-21 03:46:03 +0400

    day 20 part 1, Showing alert messages

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,50 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        
-        Button {
-            print("Edit button was tapped")
-        } label: {
-            Label("Edit", systemImage: "pencil")
+        Button("Show alert") {
+            showingAlert = true
         }
+        .padding()
+        .foregroundColor(.white)
+        .background(.red)
+        .cornerRadius(15)
         
-        // если кнопка зальется цветом и нужно вернуть оригинальное изображение
-//        .renderingMode(.original)
-        
-//        Button {
-//            print("Button was tapped")
-//        } label: {
-//            Text("Tap me!")
-//                .padding()
-//                .foregroundColor(.white)
-//                .background(.green)
-//                .cornerRadius(10)
-//        }
-        
-//        VStack {
-//            Button("Button 1") { }
-//                .buttonStyle(.bordered)
-//            Button("Button 2", role: .destructive) { }
-//                .buttonStyle(.bordered)
-//            Button("Button 3", role: .destructive) { }
-//                .buttonStyle(.borderedProminent)
-//                .tint(.mint)
-//            Button("Button 4", role: .destructive) { }
-//                .buttonStyle(.borderedProminent)
-//        }
-        
-        // можно выполнять какое-то действие в кложуре
-//        Button("Do something") {
-//            print("tapped button")
-//        }
-        
-//        Button("Delete selection", role: .destructive, action: executeDelete)
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) { }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Pleace read this")
+        }
     }
-    // можно выносить action для кнопки в отдельный метод
-    //    func executeDelete() {
-    //        print("Now deleting")
-    //    }
 }
 
 struct ContentView_Previews: PreviewProvider {
