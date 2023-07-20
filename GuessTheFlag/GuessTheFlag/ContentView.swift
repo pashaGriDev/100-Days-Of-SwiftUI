f84c8ff2543b8450ad48ffe5be9dffb445d79305
Pavel Gri <zlibabaika@gmail.com>
2023-07-21 03:26:00 +0400

    day 20 part 1, Buttons and images

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,17 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
-            
-//        LinearGradient(gradient: Gradient(stops: [
-//            Gradient.Stop(color: .white, location: 0.45),
-//            Gradient.Stop(color: .black, location: 0.55)
-//        ]), startPoint: .top, endPoint: .bottom)
+        Button {
+            print("Edit button was tapped")
+        } label: {
+            Label("Edit", systemImage: "pencil")
+        }
         
-//        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+        // если кнопка зальется цветом и нужно вернуть оригинальное изображение
+//        .renderingMode(.original)
         
-        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .blue, .purple, .orange, .red]), center: .center)
+//        Button {
+//            print("Button was tapped")
+//        } label: {
+//            Text("Tap me!")
+//                .padding()
+//                .foregroundColor(.white)
+//                .background(.green)
+//                .cornerRadius(10)
+//        }
+        
+//        VStack {
+//            Button("Button 1") { }
+//                .buttonStyle(.bordered)
+//            Button("Button 2", role: .destructive) { }
+//                .buttonStyle(.bordered)
+//            Button("Button 3", role: .destructive) { }
+//                .buttonStyle(.borderedProminent)
+//                .tint(.mint)
+//            Button("Button 4", role: .destructive) { }
+//                .buttonStyle(.borderedProminent)
+//        }
+        
+        // можно выполнять какое-то действие в кложуре
+//        Button("Do something") {
+//            print("tapped button")
+//        }
+        
+//        Button("Delete selection", role: .destructive, action: executeDelete)
     }
+    // можно выносить action для кнопки в отдельный метод
+    //    func executeDelete() {
+    //        print("Now deleting")
+    //    }
 }
 
 struct ContentView_Previews: PreviewProvider {
