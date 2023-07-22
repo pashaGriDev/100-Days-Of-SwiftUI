6c63cf73b052fd2cbfce82829cdd86eb08ebf7eb
Pavel Gri <zlibabaika@gmail.com>
2023-07-22 15:54:02 +0400

    Day 21 part 2, Upgrading our design

---
+++ GuessTheFlag/GuessTheFlag/Color + ext.swift
@@ -0,0 +1,21 @@
+//
+//  Color + ext.swift
+//  GuessTheFlag
+//
+//  Created by Павел Грицков on 22.07.23.
+//
+
+import SwiftUI
+
+extension Color {
+    static var mutedBlue: Color {
+        Color(red: 0.1, green: 0.2, blue: 0.45)
+    }
+    
+    static var mutedRed: Color {
+        Color(red: 0.76, green: 0.15, blue: 0.26)
+    }
+    
+    // or
+//    static let mutedGreen: Color = .init(red: 0.1, green: 0.1, blue: 0.1)
+}
--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -21,31 +21,57 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                .init(color: .mutedBlue, location: 0.3),
+                .init(color: .mutedRed, location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
             
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                        .font(.subheadline.weight(.heavy))
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
+            VStack {
+                Spacer()
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                        // уберает выделение синим когда изображение подсвечено
-                            .renderingMode(.original)
-                            .clipShape(Capsule()) // радиус типа каплуса
-                            .shadow(radius: 5)
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                        // пропускает цвет фона немного прозрачный
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                            // уберает выделение синим когда изображение подсвечено
+                                .renderingMode(.original)
+                                .clipShape(Capsule()) // радиус типа каплуса
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score \(score)")
+                    .font(.title.bold())
+                    .foregroundColor(.white)
+                
+                Spacer()
             }
+            // задает отступы по бокам
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
@@ -61,7 +87,7 @@ struct ContentView: View {
         } else {
             scoreTitle = "Wrong"
         }
-        
+        // как я понимаю это свойство вызывает алерт
         showingScore = true
     }
     
