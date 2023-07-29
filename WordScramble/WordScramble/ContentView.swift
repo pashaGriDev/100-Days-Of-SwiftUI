9aa8ddf9d5b92c43486b3ce3cc76a6f95b2b63bd
Pavel Gri <zlibabaika@gmail.com>
2023-07-29 04:11:43 +0400

    Day 29, Adding to a list of words. Running code when our app launches

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,18 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Lola", "Alex", "Ray", "Bob"]
+    @State private var userWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     
     var body: some View {
-        List {
-            Text("Static row")
-            
-            ForEach(people, id: \.self) {
-                Text($0)
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your text", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    ForEach(userWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
             }
-            
-            Text("Static row")
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord) // почитать доку
+            .onAppear(perform: startgame) // почитать доку
         }
+    }
+    
+    func startgame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load file start.txt from bundle")
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            userWords.insert(newWord, at: 0)
+        }
+        newWord = ""
     }
 }
 
