7db8c6d8ff26b5ab2497ade79f44dd11a141d032
Pavel Gri <zlibabaika@gmail.com>
2023-08-04 02:55:26 +0400

    Day 40, Formatting our mission view

--- Moonshot/Moonshot/Mission.swift
+++ Moonshot/Moonshot/Mission.swift
@@ -8,13 +8,25 @@
 import Foundation
 
 struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
-}
-
-struct CrewRole: Codable {
-    let name: String
-    let role: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
 }
