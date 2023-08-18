13631752c1fc32aafd407419e5496e1c30567889
Pavel Gri <zlibabaika@gmail.com>
2023-08-18 20:47:18 +0400

    Day 50, Taking basic order details

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,73 +7,39 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
+    @StateObject var order = Order()
     
-    @State private var username = ""
-    @State private var email = ""
-
     var body: some View {
-        
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                Button("Create account") {
-                    print("Creating account…")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation()) // анимирует появление
+
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery details")
+                    }
                 }
             }
-            .disabled(disableForm)
-            
-        }
-        
-        
-//        AsyncImageView()
-        
-//        List(results, id: \.trackId) { item in
-//            VStack(alignment: .leading) {
-//                Text(item.trackName)
-//                    .font(.headline)
-//                Text(item.collectionName)
-//            }
-//        }
-//        .task {
-//            await loadData()
-//        }
-    }
-    
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
-    }
-    
-    func loadData() async {
-
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
+            .navigationTitle("Cupcake Corner")
         }
     }
 }
