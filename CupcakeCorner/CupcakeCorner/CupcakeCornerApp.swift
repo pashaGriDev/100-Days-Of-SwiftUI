f59b4ac5e16e06e05915d5be3b6e0263fd38463b
Pavel Gri <zlibabaika@gmail.com>
2023-08-18 19:05:10 +0400

    Day 49, Sending and receiving Codable data with URLSession and SwiftUI

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Павел Грицков on 17.08.23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
