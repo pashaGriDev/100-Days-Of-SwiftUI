aebcda264685b5b0b8823746ff24f718c4fad41c
Pavel Gri <zlibabaika@gmail.com>
2023-08-18 19:26:36 +0400

    Day 49, Loading an image from a remote server

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -21,17 +21,40 @@ struct ContentView: View {
     @State private var results = [Result]()
 
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-//                    .lineLimit(1)
-                Text(item.collectionName)
+        VStack {
+            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                ProgressView()
             }
+            .frame(width: 200, height: 200)
+            
+            AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } else if phase.error != nil {
+                    Text("There was an error loading the image.")
+                } else {
+                    ProgressView()
+                }
+            }
+            .frame(width: 200, height: 200)
         }
-        .task {
-            await loadData()
-        }
+        
+//        List(results, id: \.trackId) { item in
+//            VStack(alignment: .leading) {
+//                Text(item.trackName)
+//                    .font(.headline)
+//                Text(item.collectionName)
+//            }
+//        }
+//        .task {
+//            await loadData()
+//        }
     }
     
     func loadData() async {
