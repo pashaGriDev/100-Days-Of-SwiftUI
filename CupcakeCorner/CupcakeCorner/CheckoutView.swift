a113d9f1aa60f4a85957f4a06bd894412942ae96
Pavel Gri <zlibabaika@gmail.com>
2023-08-19 16:17:19 +0400

    Day 50, Checking for a valid address

---
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,40 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Павел Грицков on 18.08.23.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Check out")
+                }
+            }
+            .disabled(order.hasValidAddress)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
