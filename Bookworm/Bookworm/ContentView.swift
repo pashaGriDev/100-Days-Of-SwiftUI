1ded07b16672f6f452035de2a53843eb24663138
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2023-09-12 00:06:11 +0400

    day 53, How to combine Core Data and SwiftUI

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -8,10 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("notes") private var notes = ""
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
 
     var body: some View {
-        TextEditorView(notes: $notes)
+        VStack {
+            List(students) { student in
+                Text(student.name ?? "Unknow")
+            }
+            
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+                
+                let student = Student(context: moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? moc.save()
+            }
+        }
     }
 }
 
---
+++ Bookworm/Bookworm/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Павел Грицков on 11.09.23.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
