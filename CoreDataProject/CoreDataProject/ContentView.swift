86cab706e054037d4d280c48a400a061f5deb05d
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2023-09-17 15:19:47 +0400

    day 58

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -10,34 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: [],
-                  predicate: NSPredicate(format: "universe == %@", "Star Wars")) var ships: FetchedResults<Ship>
     
     var body: some View {
         VStack {
-            List(ships, id: \.self) { ship in
-                Text(ship.name ?? "Unknow name")
-            }
             
-            Button("Add examples") {
-                let ship = Ship(context: moc)
-                ship.name = "Enterprise"
-                ship.universe = "Star Trek"
-                
-                let ship2 = Ship(context: moc)
-                ship2.name = "Defiant"
-                ship2.universe = "Star Trek"
-                
-                let ship3 = Ship(context: moc)
-                ship3.name = "Millenium Falcon"
-                ship3.universe = "Star Wars"
-                
-                let ship4 = Ship(context: moc)
-                ship4.name = "Executer"
-                ship4.universe = "Star Wars"
-                
-                try? moc.save()
-            }
         }
     }
 }
