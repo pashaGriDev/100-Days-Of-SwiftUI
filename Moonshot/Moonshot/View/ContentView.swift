d8722d7bb20dbd579201e8a02c7af9aa6777ec4e
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2023-09-19 10:43:05 +0400

    day 42, added button to swich grid size

--- Moonshot/Moonshot/View/ContentView.swift
+++ Moonshot/Moonshot/View/ContentView.swift
@@ -11,14 +11,20 @@ struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
-    let colums = [
-        GridItem(.adaptive(minimum: 150))
-    ]
+    @State private var showingInline = false
+    
+    var colums2: [GridItem] {
+        if showingInline {
+            return [GridItem(.adaptive(minimum: 280))]
+        } else {
+            return [GridItem(.adaptive(minimum: 140))]
+        }
+    }
 
     var body: some View {
         NavigationStack {
             ScrollView {
-                LazyVGrid(columns: colums) {
+                LazyVGrid(columns: colums2) {
                     ForEach(missions) { mission in
                         NavigationLink {
                             MissionView(mission: mission, astronauts: astronauts)
@@ -55,6 +61,14 @@ struct ContentView: View {
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                Button {
+                    showingInline.toggle()
+                } label: {
+                    Image(systemName: showingInline ? "circle.grid.3x3.fill" : "lineweight")
+                        .foregroundColor(Color.white)
+                }
+            }
         }
     }
 }
